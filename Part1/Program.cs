9f0536318bcd6697883c5d19194f08944e391107
odmin <dizz74@mail.ru>
2021-11-16 15:52:35 +0500

    Add project files.

---
+++ Part1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Part1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Введите 3 стороны:");
+            int a = Convert.ToInt32(Console.ReadLine());
+            int b = Convert.ToInt32(Console.ReadLine());
+            int c = Convert.ToInt32(Console.ReadLine());
+
+            double S = SqrBy3(a, b, c);
+            
+            if (S == 0 || Double.IsNaN(S)) Console.WriteLine("Неверные исх.данные");
+            else
+            Console.WriteLine("Площадь:{0:f4}", S);
+            Console.ReadKey();
+        }
+
+
+        static double SqrBy3(int a, int b, int c) {
+            double p=((double)(a+b+c))/2;
+            return  Math.Sqrt(p*(p-a)*(p-b)*(p-c));
+        }
+    }
+}
---
+++ Part2/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Part2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите длину ребpa");
+            int a = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Объем куба:{0:f0} Площадь: {1:f0}",GetV(a),GetS(a));
+        }
+
+        static double GetS(int a) {
+            return a * a * 6;
+        }
+        static double GetV(int a)
+        {
+            return Math.Pow(a, 3);
+        }
+    }
+}
